b35bc3869face91c67d61ae2be867c4987ee0b49
mvs123331 <mvs123331@gmail.com>
2026-04-05 14:45:45 +0400

    Добавил в ClientService логику обновления данных клиента

--- deal/src/main/java/org/neoflex/deal/service/ClientService.java
+++ deal/src/main/java/org/neoflex/deal/service/ClientService.java
@@ -3,12 +3,13 @@ package org.neoflex.deal.service;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.neoflex.deal.dto.FinishRegistrationRequestDto;
+import org.neoflex.deal.mapper.EmploymentMapper;
 import org.neoflex.deal.model.Client;
 import org.neoflex.deal.model.jsonb.Passport;
 import org.neoflex.deal.repository.ClientRepository;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.util.UUID;
 
 @Service
@@ -18,7 +19,9 @@ public class ClientService {
 
     private final ClientRepository clientRepository;
 
-    public void updateClientPassport(UUID clientId, String issueBranch, LocalDate issueDate) {
+    private final EmploymentMapper employmentMapper;
+
+    public void updateClient(UUID clientId, FinishRegistrationRequestDto request) {
 
         Client client = clientRepository.findById(clientId)
                 .orElseThrow(() -> new EntityNotFoundException(
@@ -26,13 +29,17 @@ public class ClientService {
                 );
 
         Passport passport = client.getPassport();
-        passport.setIssueBranch(issueBranch);
-        passport.setIssueDate(issueDate);
+        passport.setIssueBranch(request.getPassportIssueBranch());
+        passport.setIssueDate(request.getPassportIssueDate());
         client.setPassport(passport);
+
+        client.setEmployment(employmentMapper.toEmployment(request.getEmployment()));
+        client.setGender(request.getGender());
+        client.setMaritalStatus(request.getMaritalStatus());
 
         clientRepository.save(client);
 
         log.info("Обновлен паспорт клиента: id={}, issueBranch={}, issueDate={}",
-                client.getClientId(), issueBranch, issueDate);
+                client.getClientId(), request.getPassportIssueBranch(), request.getPassportIssueDate());
     }
 }
